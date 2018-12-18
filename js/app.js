2baf87cef7dac2a7b6fe8c32783ae262cd243200
Hrbatovic <e.hrbatovic@accenture.com>
2018-12-18 22:29:19 +0100

    add angular model

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 var app = angular.module("myApp", []);
+
+//naming convention is camelCase and results in a my-directive directive
+app.directive("myDirective", function(){
+    return{
+        //you return a template with html elements in the directive
+
+        template: "<p> This content comes from a custom directive : my-directive</p>"
+    };
+});
